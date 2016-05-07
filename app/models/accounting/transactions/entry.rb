a51a542527b2c32d6c0f93e1afb8a75f36618ac5
Von Christian Halip <vc.halip@gmail.com>
2016-05-07 10:18:07 +0800

    Update entry specs.

--- app/models/accounting/transactions/entry.rb
+++ app/models/accounting/transactions/entry.rb
@@ -3,7 +3,30 @@ module Accounting
     class Entry < ApplicationRecord
       belongs_to :entriable, polymorphic: true
       belongs_to :recorder, class_name: "User", foreign_key: 'recorder_id'
+
+      has_many :credit_amounts, :extend => Accounting::AmountsExtension, :class_name => 'Accounting::CreditAmount', :inverse_of => :entry, dependent: :destroy
+      has_many :debit_amounts, :extend => Accounting::AmountsExtension, :class_name => 'Accounting::DebitAmount', :inverse_of => :entry, dependent: :destroy
+      has_many :credit_accounts, :through => :credit_amounts, :source => :account, :class_name => 'Accounting::Accounts::Account', dependent: :destroy
+      has_many :debit_accounts, :through => :debit_amounts, :source => :account, :class_name => 'Accounting::Accounts::Account', dependent: :destroy
+
       validates :date, :description, presence: true
+      validate :has_credit_amounts?
+      validate :has_debit_amounts?
+      validate :amounts_cancel?
+      accepts_nested_attributes_for :debit_amounts, :credit_amounts
+
+      private
+      def has_credit_amounts?
+        errors[:base] << "Entry must have at least one credit amount" if self.credit_amounts.blank?
+      end
+
+      def has_debit_amounts?
+        errors[:base] << "Entry must have at least one debit amount" if self.debit_amounts.blank?
+      end
+
+      def amounts_cancel?
+        errors[:base] << "The credit and debit amounts are not equal" if credit_amounts.balance_for_new_record != debit_amounts.balance_for_new_record
+      end
     end
   end
 end
--- spec/models/accounting/transactions/entry_spec.rb
+++ spec/models/accounting/transactions/entry_spec.rb
@@ -4,10 +4,19 @@ RSpec.describe Accounting::Transactions::Entry, :type => :model do
   describe "associations" do
     it { is_expected.to belong_to :entriable }
     it { is_expected.to belong_to :recorder }
+    it { is_expected.to have_many :credit_amounts }
+    it { is_expected.to have_many :debit_amounts }
+    it { is_expected.to have_many :debit_accounts }
+    it { is_expected.to have_many :credit_accounts }
   end
 
   describe "validations" do
     it { is_expected.to validate_presence_of :date }
     it { is_expected.to validate_presence_of :description }
+  end
+
+  describe "nested_attributes" do
+    it { is_expected.to accept_nested_attributes_for(:debit_amounts) }
+    it { is_expected.to accept_nested_attributes_for(:credit_amounts) }
   end
 end
