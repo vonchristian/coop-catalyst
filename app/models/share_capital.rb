4b803b7aa3e3e52af30d0ccd9c04653527007e5d
Von Christian Halip <vc.halip@gmail.com>
2016-05-21 22:04:54 +0800

    Add balance method to share capital.

--- app/models/share_capital.rb
+++ app/models/share_capital.rb
@@ -4,4 +4,20 @@ class ShareCapital < ApplicationRecord
 
   validates :date_opened, :account_number, presence: true
   validates :account_number, uniqueness: true
+  def balance(hash={})
+      if hash[:from_date] && hash[:to_date]
+        from_date = hash[:from_date].kind_of?(Date) ? hash[:from_date] : Date.parse(hash[:from_date])
+        to_date = hash[:to_date].kind_of?(Date) ? hash[:to_date] : Date.parse(hash[:to_date])
+        self.deposits.where(date: from_date..to_date).map{|a| a.debit_amounts.balance}.sum
+      else
+       self.total_deposits
+      end
+    end
+  def deposits
+    Accounting::Accounts::Equity.find_by_name!("Share Capital").entries.where(entriable: self)
+  end
+  def total_deposits
+    self.deposits.map{|a| a.credit_amounts.balance}.sum
+  end
+
 end
