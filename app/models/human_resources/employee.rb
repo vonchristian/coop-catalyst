a23f6ad3b7a1b077b3bafff10d30547a819c8a92
Von Christian Halip <vc.halip@gmail.com>
2016-05-21 22:41:48 +0800

    WIP: Improve sti of users/member/employee.

--- app/models/human_resources/employee.rb
+++ app/models/human_resources/employee.rb
@@ -2,19 +2,21 @@ module HumanResources
   class Employee < User
       belongs_to :role, foreign_key: 'role_id'
       has_many :approved_loans, class_name: "LoansSection::Approval", foreign_key: 'approver_id'
+
       def self.matches_role(role)
         self.where(role: role)
       end
+
       def self.can_approve_loan?
-        self.select{|a| a.loan_officer? || a.manager? }
+        self.select{ |a| a.loan_officer? || a.manager? }
       end
 
       def self.loan_officers
-        self.select{|a| a.loan_officer? }
+        self.select{ |a| a.loan_officer? }
       end
 
       def self.managers
-        self.select{|a| a.manager?}
+        self.select{ |a| a.manager? }
       end
 
       def approved_loan(loan)
