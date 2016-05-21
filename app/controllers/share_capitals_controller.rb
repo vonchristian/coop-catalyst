7f7b2c75f9940dd7aa5eff803ad5fed52290771a
Von Christian Halip <vc.halip@gmail.com>
2016-05-21 22:05:32 +0800

    redirect to account owner after creating a share capital account.

--- app/controllers/share_capitals_controller.rb
+++ app/controllers/share_capitals_controller.rb
@@ -5,9 +5,9 @@ class ShareCapitalsController  < ApplicationController
   end
   def create
     @user = User.find(params[:user_id])
-    @share_capital = @user.create_share_capital(share_capital_params)
+    @share_capital = @user.build_share_capital(share_capital_params)
     if @share_capital.save
-      redirect_to @share_capital, notice: "Share Capital Account opened successfully."
+      redirect_to @user, notice: "Share Capital Account opened successfully."
     else
       render :new
     end
