a151d2f78577390ae2244bf742a1885d10f51997
Von Christian Halip <vc.halip@gmail.com>
2016-05-21 15:17:05 +0800

    Add mobile number to users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508145143) do
+ActiveRecord::Schema.define(version: 20160521070700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -203,6 +203,7 @@ ActiveRecord::Schema.define(version: 20160508145143) do
     t.datetime "photo_updated_at"
     t.string   "type"
     t.integer  "role_id"
+    t.string   "mobile_number"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["role_id"], name: "index_users_on_role_id", using: :btree
